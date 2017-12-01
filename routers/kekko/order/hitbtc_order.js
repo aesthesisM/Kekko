6d82de1e16bb503232bf8daed92a72df6dd57a4d
aesthesism <mustafaaygulis@hotmail.com>
2017-12-01 16:04:48 +0300

    db initilialized changed

--- routers/kekko/order/hitbtc_order.js
+++ routers/kekko/order/hitbtc_order.js
@@ -1,9 +1,8 @@
 var express = require('express');
-var hitBTC = require('../../../api/hitbtc/hitbtc');
 var router = express.Router();
-
+var HitBTC = require('../../../api/hitbtc/hitbtc');
 router.get('/', function (req, res, next) {
-    res.render('order/hitbtc/hitbtc_home', { title: 'HitBtc Home Page' });
+    res.render('order/hitbtc/hitbtc_home', {title: 'HitBtc Home Page'});
 });
 
 module.exports = router;
