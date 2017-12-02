8c8b82c9bec16e3a49606fcad0dff15ccf6c6f2b
aesthesism <mustafaaygulis@hotmail.com>
2017-12-02 08:32:52 +0300

    CB initialize script turned into sync method

--- routers/error.js
+++ routers/error.js
@@ -4,7 +4,7 @@ var router = express.Router();
 //enterance page
 router.get('/error', function (req, res, next) {
     console.log('index router called');
-    res.render('error', { title: 'Error': errorMessage: err });
+    res.render('error', {title: 'Error', errorMessage: err});
 });
 
 module.exports = router;
