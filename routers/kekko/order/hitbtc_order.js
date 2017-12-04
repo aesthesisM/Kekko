2aadc2289c5c0e66b6f322c45937be220d3ba8ab
aesthesism <mustafaaygulis@hotmail.com>
2017-12-04 12:47:54 +0300

    Stop loss columns added to order table

--- routers/kekko/order/hitbtc_order.js
+++ routers/kekko/order/hitbtc_order.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var hitOrderManager = require('../../../dao/order/hitbtc_order');
+var responseObject = require('../../../util/response').response;
 router.get('/', function (req, res, next) {
     res.render('order/hitbtc/hitbtc_home', {title: 'HitBtc Home Page'});
 });
@@ -8,9 +9,16 @@ router.get('/', function (req, res, next) {
 router.get('/chain/getAll', function (req, res, next) {
     hitOrderManager.hitbtc_db_getAllOrderChains(hitOrderManager.apiName, function (err, data) {
         if (err) {
-            res.send({errMessage: err});
+            responseObject.data = null;
+            responseObject.message = err;
+            responseObject.result = -1;
+            console.error(err);
+            res.send({respObj: responseObject});
         } else {
-            res.send(data);
+            responseObject.data = data;
+            responseObject.message = 'ok';
+            responseObject.result = 1;
+            res.send({respObj: responseObject});
         }
     });
 });
@@ -20,10 +28,16 @@ router.post('/chain/add', function (req, res, next) {
     //var chainOrderObj = {order_chain_name: 'BAMBAM', api_id_fk: 1};
     hitOrderManager.hitbtc_db_addOrderChain(req.body, function (err, data) {
         if (err) {
-            console.log(err);
-            res.send({errMessage: err});
+            responseObject.data = null;
+            responseObject.message = err;
+            responseObject.result = -1;
+            console.error(err);
+            res.send({respObj: responseObject});
         } else {
-            res.send(data);
+            responseObject.data = data;
+            responseObject.message = 'ok';
+            responseObject.result = 1;
+            res.send({respObj: responseObject});
         }
     });
 });
@@ -33,23 +47,90 @@ router.post('/chain/update', function (req, res, next) {
     //var chainOrderObj = {order_chain_name: 'BAMBAM', api_id_fk: 1};
     hitOrderManager.hitbtc_db_updateOrderChain(req.body, function (err, data) {
         if (err) {
-            console.log(err);
-            res.send({errMessage: err});
+            responseObject.data = null;
+            responseObject.message = err;
+            responseObject.result = -1;
+            console.error(err);
+            res.send({respObj: responseObject});
         } else {
-            res.send(data);
+            responseObject.data = data;
+            responseObject.message = 'ok';
+            responseObject.result = 1;
+            res.send({respObj: responseObject});
         }
     });
 });
 
 router.post('/chain/delete', function (req, res, next) {
-
     //var chainOrderObj = {order_chain_name: 'BAMBAM', api_id_fk: 1};
     hitOrderManager.hitbtc_db_deleteOrderChain(req.body, function (err, data) {
         if (err) {
-            console.log(err);
-            res.send({errMessage: err});
+            responseObject.data = null;
+            responseObject.message = err;
+            responseObject.result = -1;
+            console.error(err);
+            res.send({respObj: responseObject});
         } else {
-            res.send(data);
+            responseObject.data = data;
+            responseObject.message = 'ok';
+            responseObject.result = 1;
+            res.send({respObj: responseObject});
+        }
+    });
+});
+
+router.get('/chain/:chainId/getOrders', function (req, res, next) {
+
+    hitOrderManager.hitbtc_db_getOrdersByOrderChainId({order_chain_id_fk: req.params.chainId}, function (err, data) {
+        if (err) {
+            responseObject.data = null;
+            responseObject.message = err;
+            responseObject.result = -1;
+            console.error(err);
+            res.send({respObj: responseObject});
+        } else {
+            responseObject.data = data;
+            responseObject.message = 'ok';
+            responseObject.result = 1;
+            res.send({respObj: responseObject});
+        }
+    });
+});
+
+router.post('/chain/:chainId/addOrder', function (req, res, next) {
+    var orderObj = req.body;
+
+    hitOrderManager.hitbtc_db_addOrder({}, function (err, data) {
+        if (err) {
+            responseObject.data = null;
+            responseObject.message = err;
+            responseObject.result = -1;
+            console.error(err);
+            res.send({respObj: responseObject});
+        } else {
+            responseObject.data = data;
+            responseObject.message = 'ok';
+            responseObject.result = 1;
+            res.send({respObj: responseObject});
+        }
+    });
+});
+
+router.post('/chain/:chainId/updateOrder', function (req, res, next) {
+    var orderObj = req.body;
+
+    hitOrderManager.hitbtc_db_updateOrder({}, function (err, data) {
+        if (err) {
+            responseObject.data = null;
+            responseObject.message = err;
+            responseObject.result = -1;
+            console.error(err);
+            res.send({respObj: responseObject});
+        } else {
+            responseObject.data = data;
+            responseObject.message = 'ok';
+            responseObject.result = 1;
+            res.send({respObj: responseObject});
         }
     });
 });
