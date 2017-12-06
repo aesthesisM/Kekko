71ddd79c7c17689dde549d3a54567563c7a0f65f
aesthesism <mustafaaygulis@hotmail.com>
2017-12-06 22:25:29 +0300

    Websocket api added to Kekko. And it configured to test hitbtc socket api with ethbtc pair. Configured to reconnect if server drops connection.

---
+++ webSocketListener/listener.js
@@ -0,0 +1,32 @@
+const WebSocket = require('ws');
+
+var reconnectInterval = x * 1000 * 5; //5seconds
+var ws;
+var connect = function () {
+    ws = new WebSocket('wss://api.hitbtc.com/api/2/ws');
+    ws.on('open', function open() {
+        console.log('connected');
+        var obj = {
+            "method": "subscribeTicker",
+            "params": {
+                "symbol": "ETHBTC"
+            },
+            "id": 123
+
+        };
+        ws.send(JSON.stringify(obj));
+    });
+    ws.on('error', function () {
+        console.log('socket error');
+    });
+    ws.on('close', function () {
+        console.log('socket close');
+        setTimeout(connect, reconnectInterval);
+    });
+    ws.on('message', function incoming(data) {
+        console.log(data);
+    });
+};
+
+connect();
+
