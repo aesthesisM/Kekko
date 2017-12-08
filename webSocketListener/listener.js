23be94dc16c0d91814a4eef3d789bd2ab049b106
aesthesism <mustafaaygulis@hotmail.com>
2017-12-08 13:52:53 +0300

    local config

--- webSocketListener/listener.js
+++ webSocketListener/listener.js
@@ -1,15 +1,15 @@
 const WebSocket = require('ws');
 
-var reconnectInterval = x * 1000 * 5; //5seconds
+var reconnectInterval = 1000 * 5; //5seconds
 var ws;
-var connect = function () {
+var connect = function (kill) {
     ws = new WebSocket('wss://api.hitbtc.com/api/2/ws');
     ws.on('open', function open() {
         console.log('connected');
         var obj = {
             "method": "subscribeTicker",
             "params": {
-                "symbol": "ETHBTC"
+                "symbol": "ETHUSD"
             },
             "id": 123
 
@@ -23,12 +23,13 @@ var connect = function () {
     */
     ws.on('close', function () {
         console.log('socket close');
-        setTimeout(connect, reconnectInterval);
+        if (kill != true)
+            setTimeout(connect, reconnectInterval);
     });
     ws.on('message', function incoming(data) {
         console.log(data);
     });
 };
 
-connect();
+connect(false);
 
