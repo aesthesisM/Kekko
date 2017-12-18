be489f6b11f5f4679abb8989370153686c8f08c1
aesthesism <mustafaaygulis@hotmail.com>
2017-12-18 13:24:29 +0300

    temporary committed

--- api/socket/hitbtc.js
+++ api/socket/hitbtc.js
@@ -4,72 +4,22 @@ var queryString = require('querystring');
 var async = require('async');
 var WebSocket = require('ws');
 
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
+var hitBTCClient;
 
+var orderListener = new wsOrderListener(console.log);
+var pairListener = new wsPairListener(console.log);
 
-ws = new WebSocket('wss://api.hitbtc.com/api/2/ws', null, {
-    handshakeTimeout: 12000
-});
-
-var ws2 = new WebSocket('wss://api.hitbtc.com/api/2/ws', null, {
-    handshakeTimeout: 12000
-});
-
-var ws3 = new WebSocket('wss://api.hitbtc.com/api/2/ws', null, {
-    handshakeTimeout: 12000
-});
-
-
-
-var wsOrderListener = function (obj, callback) {
-    ws3.send(JSON.stringify(obj));
-    ws3.on('open', function () {
-        console.log('Websocket opened');
+//Socket Pair Listener
+function wsPairListener(callback) {
+    var ws;
+    ws = new WebSocket('wss://api.hitbtc.com/api/2/ws', null, {
+        handshakeTimeout: 12000
     });
-
-    ws3.on('error', function (err) {
-        callback(err);
-        console.error(err);
-    });
-
-    ws3.on('close', function () {
-        console.log('Websocket closed');
-    });
-
-    ws3.on('message', function (data) {
-        callback(null, data);
-    });
-}
-
-var wsOrderManager = function (obj, callback) {
-    ws2.send(JSON.stringify(obj));
-    ws2.on('open', function () {
-        console.log('Websocket opened');
-    });
-
-    ws2.on('error', function (err) {
-        callback(err);
-        console.error(err);
-    });
-
-    ws2.on('close', function () {
-        console.log('Websocket closed');
-    });
-
-    ws2.on('message', function (data) {
-        callback(null, data);
-    });
-}
-
-
-
-
-
-var wsHitBTC = function (obj, callback) {
-    ws.send(JSON.stringify(obj));
     ws.on('open', function () {
-        console.log('Websocket opened');
+        console.log('WSOrderListener Websocket opened');
+    });
+    ws.on('close', function () {
+        console.log('WSOrderListener Websocket closed');
     });
 
     ws.on('error', function (err) {
@@ -77,16 +27,86 @@ var wsHitBTC = function (obj, callback) {
         console.error(err);
     });
 
+    ws.on('message', function (data) {
+        callback(null, data);
+    });
+
+    this.ws = ws;
+}
+
+wsPairListener.prototype._listen = function (pair, listening) {
+    var obj = {
+        "method": "subscribeTicker",
+        "params": {
+            "symbol": pair
+        },
+        "id": "Kekko"
+    }
+    if (!listening) {
+        obj.method = "unsubscribeTicker";
+        console.log("closed ears for " + pair);
+    } else {
+        console.log("listening " + pair);
+    }
+    this.ws.send(JSON.stringify(obj));
+
+}
+
+wsPairListener.prototype._authorize = function (data) {
+    this.ws.send(JSON.stringify(data));
+}
+
+wsPairListener.prototype._close = function () {
+    this.ws.close();
+}
+
+
+//Socket Order Listener
+function wsOrderListener(callback) {
+    var ws = new WebSocket('wss://api.hitbtc.com/api/2/ws', null, {
+        handshakeTimeout: 12000
+    });
+
+    ws.on('open', function () {
+        console.log('wsPairListener connected');
+    });
+
     ws.on('close', function () {
-        console.log('Websocket closed');
+        console.log('wsPairListener Websocket closed');
+    });
+
+    ws.on('error', function (err) {
+        callback(err);
+        console.error('WSOrderListener error:' + err);
     });
 
     ws.on('message', function (data) {
         callback(null, data);
-        //eventEmitter.removeListener('message',this);
     });
-
     
+    this.ws = ws;
+}
+
+wsOrderListener.prototype._authorize = function (data) {
+    this.ws.send(JSON.stringify(data));
+}
+
+wsOrderListener.prototype._listenOrders = function (listen) {
+    //order status = new, suspended, partiallyFilled, filled, canceled, expired
+    var obj = {
+        "method": "subscribeReports",
+        "params": {}
+    }
+
+    if (!listen) {
+        obj.method = "unsubscribeReports";
+    }
+    this.ws.send(JSON.stringify(obj));
+
+}
+
+wsPairListener.prototype._close = function () {
+    this.ws.close();
 }
 
 function HitBTCClient(APIKey, APISecret) {
@@ -115,27 +135,26 @@ HitBTCClient.prototype._authorize = function (callback) {
     return authObj;
 }
 
-HitBTCClient.prototype._activeOrders = function (callback) {
-
-    var activeOrdersObj = {};
-    activeOrdersObj.method = "subscribeReports";
-    activeOrdersObj.params = {};
-    return activeOrdersObj;
-}
-
-var a = new HitBTCClient('f6162add43cb90c6750b111feeed0010', 'de7bf2ff1889ab9edd9cfad906fda42b');
-
 setTimeout(function () {
+    hitBTCClient = new HitBTCClient('f6162add43cb90c6750b111feeed0010', 'de7bf2ff1889ab9edd9cfad906fda42b');
     //1 authenticate
-    var authObj = a._authorize();
-    //wsHitBTC(authObj, console.log);
-    wsOrderListener(authObj, console.log);
-    //wsOrderManager(authObj, console.log);
-    wsOrderListener(authObj, function (err, data) {
-        console.log("BAMBAM");
-    })
-    /*wsOrderListener(a._activeOrders(), function (err, data) {
-        //console.error(data);
-    })*/
+    var authObj = hitBTCClient._authorize();
+
+    orderListener._authorize(authObj);
+    pairListener._authorize(authObj);
+
+    pairListener._listen("EOSUSD", true);
+    pairListener._listen("XRPUSDT", true);
+    orderListener._listenOrders(true);
+    setTimeout(function () {
+        pairListener._listen("EOSUSD", false);
+        pairListener._listen("XRPUSDT", false);
+        orderListener._listenOrders(false);
+    }, 5000);
 
 }, 3000);
+
+exports = wsOrderListener;
+exports = wsPairListener;
+
+
