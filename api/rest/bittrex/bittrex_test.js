f310e3076f04f225227108585021414d5e9da6b9
aesthesism <mustafaaygulis@hotmail.com>
2018-01-01 14:40:43 +0300

    CCI has been implemented.

--- api/rest/bittrex/bittrex_test.js
+++ api/rest/bittrex/bittrex_test.js
@@ -1,32 +1,39 @@
 var Bittrex = require('./bittrex');
 var async = require('async');
 //hour
-var longTermCount = 200;
-var midTermCount = 90;
-var shortTermCount = 40;
-var quickTermCount = 9;
+
+//MA
+var MA_longTermPeriod = 200;
+var MA_midTermPeriod = 90;
+var MA_shortTermPeriod = 40;
+var MA_quickTermPeriod = 9;
+
+//CCI
+var CCI_period = 14;
+var CCI_Constant = 0.015;
+//
+var pairs = [];
+var signals = [];
+var runnerPairCount = 0;
 
 var bittrexClient = new Bittrex();
-var watch = [];
+
 function checkMA(data, pair) { //calculate depending on C which means close price
-    if (data == undefined || data.length == 0) {
-        console.log("socket null data");
+    if (data.length < MA_longTermPeriod) { //new added coins doesnt have enough data to check
         return;
-    } else if (pair != null) {
-        console.log("working pair :" + pair);
     }
-    var longTermAvarage = 0, midTermAvarage = 0, shortTermAvarage = 0, quickTermAvarage = 0, length = data.length - longTermCount - 1;
+    var longTermAvarage = 0, midTermAvarage = 0, shortTermAvarage = 0, quickTermAvarage = 0, length = data.length - MA_longTermPeriod;
     var long_mid_difference = 0, mid_short_difference = 0, short_quick_difference = 0;
     var max = 0, min = 1;
     //first checking MA
-    for (var i = 0; i <= longTermCount; i++) {
-        if (longTermCount - i < quickTermCount) {
+    for (var i = 0; i < MA_longTermPeriod; i++) {
+        if (MA_longTermPeriod - i < MA_quickTermPeriod) {
             quickTermAvarage += data[length + i].C;
         }
-        if (longTermCount - i < shortTermCount) {
+        if (MA_longTermPeriod - i < MA_shortTermPeriod) {
             shortTermAvarage += data[length + i].C;
         }
-        if (longTermCount - i < midTermCount) {
+        if (MA_longTermPeriod - i < MA_midTermPeriod) {
             midTermAvarage += data[length + i].C;
         }
 
@@ -40,10 +47,10 @@ function checkMA(data, pair) { //calculate depending on C which means close pric
 
     }
 
-    longTermAvarage /= longTermCount;
-    midTermAvarage /= midTermCount;
-    shortTermAvarage /= shortTermCount;
-    quickTermAvarage /= quickTermCount;
+    longTermAvarage /= MA_quickTermPeriod;
+    midTermAvarage /= MA_midTermPeriod;
+    shortTermAvarage /= MA_shortTermPeriod;
+    quickTermAvarage /= MA_quickTermPeriod;
 
     long_mid_difference = ((longTermAvarage - midTermAvarage) / midTermAvarage) * 100;
     mid_short_difference = ((midTermAvarage - shortTermAvarage) / shortTermAvarage) * 100;
@@ -51,29 +58,71 @@ function checkMA(data, pair) { //calculate depending on C which means close pric
 
     if (longTermAvarage > midTermAvarage && midTermAvarage > shortTermAvarage && shortTermAvarage > quickTermAvarage) {
         if (long_mid_difference > 9 && mid_short_difference > 9 && short_quick_difference > 9) { //last part of dump
-            console.log("Perpect Match!!! Last part of dump!");
-            console.log("pair : " + pair + " | longTerm :" + longTermAvarage + " | midTerm :" + midTermAvarage + " | shortTerm :" + shortTermAvarage + " | \nquicTerm :" + quickTermAvarage + " | max : " + max + " | min : " + min + " | enterancePrice :" + data[data.length - 1].L);
+            var signalObj = {
+                "pair": pair,
+                "type": "LPOD (last part of dump)",
+                "longTermAvarage": longTermAvarage,
+                "midTermAvarage": midTermAvarage,
+                "shortTermAvarage": shortTermAvarage,
+                "quickTermAvarage": quickTermAvarage,
+                "longTermMin": min,
+                "longTermMax": max,
+                "CCI": 0,
+                "action": 0
+            }
+            signals[pair] = signalObj;
         } else if (long_mid_difference < 4, mid_short_difference < 4, short_quick_difference < 4) { //normal cycle of coin
-            console.log("Perfect Match!!! Normal cycle of price")
-            console.log("pair : " + pair + " | longTerm :" + longTermAvarage + " | midTerm :" + midTermAvarage + " | shortTerm :" + shortTermAvarage + " | \nquicTerm :" + quickTermAvarage + " | max : " + max + " | min : " + min + " | enterancePrice :" + data[data.length - 1].L);
+            var signalObj = {
+                "pair": pair,
+                "type": "NCOP (normal cycle of price)",
+                "longTermAvarage": longTermAvarage,
+                "midTermAvarage": midTermAvarage,
+                "shortTermAvarage": shortTermAvarage,
+                "quickTermAvarage": quickTermAvarage,
+                "longTermMin": min,
+                "longTermMax": max,
+                "CCI": 0,
+                "action": 0
+            }
+            signals[pair] = signalObj;
         }
     }
 
 }
 
 function checkCCI(data, pair) {
+    if (signals[pair] == undefined || signals[pair] == null) {
+        return;
+    }
+    var mean = 0;
+    var meanDeviation = 0;
+    var CCIRate = 0;
 
+    for (var i = 0; i < CCI_period; i++) {
+        mean += data[data.length - CCI_period + i].C;
+    }
+    mean /= CCI_period;
+
+    for (var i = 0; i < CCI_period; i++) {
+        meanDeviation += Math.abs(mean - data[data.length - CCI_period + i].C)
+    }
+    meanDeviation /= CCI_period;
+
+    CCIRate = (data[data.length - 1].C - mean) / (CCI_Constant * meanDeviation);
+
+    signals[pair]["CCI"] = CCIRate;
+    console.log(signals[pair]);
 }
 
 
 function runIndicators(data, pair) {
+    if (data == undefined || data.length == 0) {
+        console.log("socket null data");
+        return;
+    }
     checkMA(data, pair);
-    //checkCCI(data, pair);
+    checkCCI(data, pair);
 }
-
-var pairs = [];
-var signals = [];
-var j = 0;
 
 //setInterval(function () {
 if (pairs.length == 0) {
@@ -88,12 +137,12 @@ if (pairs.length == 0) {
                         pairs.push(data.result[i]["MarketName"]);
                     }
                 }
-                bittrexClient._getHistoricalData({ marketName: pairs[j], tickInterval: "thirtyMin", _: new Date().getTime() }, recursive);
+                bittrexClient._getHistoricalData({ marketName: pairs[runnerPairCount], tickInterval: "thirtyMin", _: new Date().getTime() }, recursive);
             }
         }
     );
 } else {
-    bittrexClient._getHistoricalData({ marketName: pairs[j], tickInterval: "thirtyMin", _: new Date().getTime() }, recursive);
+    bittrexClient._getHistoricalData({ marketName: pairs[runnerPairCount], tickInterval: "thirtyMin", _: new Date().getTime() }, recursive);
 }
 // }, 1800000);//30 min
 function recursive(data, err, pair) {
@@ -101,18 +150,26 @@ function recursive(data, err, pair) {
         console.error(err);
         bittrexClient._getHistoricalData({ marketName: pair, tickInterval: "thirtyMin", _: new Date().getTime() }, recursive);
     } else {
-        runIndicators(data.result, pair);
-        if (j < pairs.length - 1) {
-            j++;
-            bittrexClient._getHistoricalData({ marketName: pairs[j], tickInterval: "thirtyMin", _: new Date().getTime() }, recursive);
+        try {
+            console.log("working pair " + pair);
+            runIndicators(data.result, pair);
+        } catch (err) {
+            console.error(err);
+            //BTC-UKG Has problem i dont know why but its not related to us
+            //UKG is a new coin and doesnt have enough long data to calculate
+            //so new control has been added for checking datas lengtt if its enough 
+        }
+        if (runnerPairCount < pairs.length - 1) {
+            runnerPairCount++;
+            bittrexClient._getHistoricalData({ marketName: pairs[runnerPairCount], tickInterval: "thirtyMin", _: new Date().getTime() }, recursive);
         } else {
             console.log("finished for now ;)");
-            j = 0;
+            runnerPairCount = 0;
         }
     }
 }
 
-//setInterval(checkSignals, 60000); //1 min
+//setInterval(checkSignals, 30000); //30 sec
 function checkSignals() {
     if (signals.length > 0) {
         for (var i = 0; i < signals.length; i++) {
