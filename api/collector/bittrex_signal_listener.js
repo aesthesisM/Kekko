d895fd97e5abb3f01ce1fe338a9f56ea93a91687
aesthesism <mustafaaygulis@hotmail.com>
2018-01-25 17:03:35 +0300

    bambam çalışıyor fakat sell durumları sapıtıyor.

--- api/collector/bittrex_signal_listener.js
+++ api/collector/bittrex_signal_listener.js
@@ -1,13 +1,14 @@
-var io = require('socket.io-client')('http://localhost:50000');
-var async = require('async');
-var Bittrex = require('../rest/bittrex/bittrex');
+var io = require("socket.io-client")("http://localhost:50000");
+var async = require("async");
+var Bittrex = require("../rest/bittrex/bittrex");
 var bittrexClient = new Bittrex();
-io.on('signal', function (data) {
-    console.log(data);
-    runSignalOrder(data);
+io.on("signal", function(data) {
+  console.log(data);
+  runSignalOrder(data);
 });
-var happenedOrders = new Array();
-var orders = new Array();
+var happenedOrders = [];
+var holdingOrders = [];
+var orders = [];
 var wallet = 1;
 
 const MAX_ORDER_COUNT = 10;
@@ -15,98 +16,122 @@ const SELL_HIGHER_PERCENT = 0.02;
 const STOP_LOSS_PERCENT = 0.02;
 var current_order_count = 0;
 
-
 function runSignalOrder(data) {
-    if (current_order_count < MAX_ORDER_COUNT) {
-        placeOrder(data, "buy", new Date().getTime() + 30 * 60 * 1000); //time+30 min for buy timeout);
-        current_order_count++;
-    }
+  if (current_order_count < MAX_ORDER_COUNT) {
+    placeOrder(data, "buy", new Date().getTime() + 30 * 60 * 1000); //time+30 min for buy timeout);
+    current_order_count++;
+  }
 }
 
 function placeOrder(data, side, timeOut) {
-    var quantity = parseFloat((wallet / (MAX_ORDER_COUNT - orders.length)) / data.lastClosePrice);
-    wallet = wallet - quantity * data.lastClosePrice;
-    quantity = quantity - (quantity * 0.0025);
-    var orderObj = {
-        "side": side,//"sell"
-        "price": data.lastClosePrice,
-        "quantity": quantity,
-        "pair": data.pair,
-        "stopLossPrice": 0,
-        "interval": "oneMin",
-        "timeOut": timeOut
-    };
+  var quantity = parseFloat(
+    wallet / (MAX_ORDER_COUNT - orders.length) / data.lastClosePrice
+  );
+  wallet = wallet - quantity * data.lastClosePrice;
+  quantity = quantity - quantity * 0.0025;
+  var orderObj = {
+    side: side, //"sell"
+    price: data.lastClosePrice,
+    quantity: quantity,
+    pair: data.pair,
+    stopLossPrice: 0,
+    interval: "oneMin",
+    timeOut: timeOut
+  };
 
-    orders.push(orderObj);
-    console.log("Current orders :\nOrderLength : " + orders.length + "\n" + JSON.stringify(orders) + "\nWallet : " + wallet);
+  orders.push(orderObj);
+  console.log(
+    "Current orders :\nOrderLength : " +
+      orders.length +
+      "\n" +
+      JSON.stringify(orders) +
+      "\nWallet : " +
+      wallet
+  );
 }
 
 function checkOrders() {
-    for (var i = 0; i < orders.length; i++) {
-        var order = orders[i];
-        var orderStatus;
-        if (order.timeOut > new Date().getTime()) {
-            async.series(
-                [
-                    function (callback) {
-                        bittrexClient._getLatestTick({ marketName: order.pair, tickInterval: order.interval }, function (data, err) {
-                            if (err) {
-                                callback(err);
-                            } else {
-                                orderStatus = data;
-                                callback();
-                            }
-                        });
-                    },
-                    function (callback) {
-                        if (order.side === "buy") {
-                            if (orderStatus.result[0].L < order.price) {//buy happened....
-                                //put place order
-                                order.side = "sell";
-                                order.price = order.price + order.price * SELL_HIGHER_PERCENT;
-                                order.stopLossPrice = order.price - order.price * STOP_LOSS_PERCENT;
-                                order.timeOut = new Date().getTime() + 8 * 30 * 60 * 1000;//sell timeout
-                                orders.splice(i--, 1);
-                                orders.push(order);
-                            }
-                        } else if (order.side === "sell") {
-                            if (order.stopLossPrice > orderStatus.result[0].L) { //stoploss worked
-                                wallet = wallet + order.price * order.quantity - order.price * order.quantity * 0.0025;
-                                orders.splice(i--, 1);
-                                current_order_count--;
-                                happenedOrders.push({ "order": order, "stopLoss": true, "wallet": wallet });
-                            } else if (order.price < orderStatus.result[0].H) { //sell happened
-                                wallet = wallet + order.price * order.quantity - order.price * order.quantity * 0.0025;
-                                orders.splice(i--, 1);
-                                current_order_count--;
-                                happenedOrders.push({ "order": order, "stopLoss": false, "wallet": wallet });
-                            }
-                        }
-                        callback();
-                    }
-                ],
-                function (err) {
-                    if (err) {
-                        console.error(err);
-                    } else {
-
-                    }
-                }
-            );
-        } else { //timeout remove order
-            orders.splice(i, 1);
-            if (order.side === "buy") {
-                wallet = wallet + order.price * order.quantity;
-            } else {
-                //hodl forever :D :D :D or sell it with current price
-            }
-        }
-
-    }
-    if (happenedOrders.length > 0)
-        console.log(happenedOrders);
+  if (orders.length > 0)
+    bittrexClient._getLatestTick(
+      {
+        marketName: orders[0].pair,
+        tickInterval: orders[0].interval,
+        _: new Date().getTime(),
+        index: 0
+      },
+      recursive
+    );
 }
 
-setInterval(function () { checkOrders() }, 1 * 60 * 1000);
+function recursive(orderStatus, err, pair, interval, index) {
+  var order = orders[index];
+  console.log(orderStatus);
+  if (orderStatus != null && orderStatus.result != null && orderStatus.result != undefined && orderStatus.result.length > 0)//if result data is not null 
+    if(order.timeOut > new Date().getTime()){
+        if (order.side === "buy") {
+        if (orderStatus.result[0].L < order.price) {
+            //buy happened....
+            //put place order
+            order.side = "sell";
+            order.price = order.price + order.price * SELL_HIGHER_PERCENT;
+            order.stopLossPrice = order.price - order.price * STOP_LOSS_PERCENT;
+            order.timeOut = new Date().getTime() + 8 * 30 * 60 * 1000; //sell timeout
+            orders.splice(index, 1);
+            index--;
+            orders.push(order);
+            console.log("buy happened"+ JSON.stringify(order));
+        }
+        } else if (order.side === "sell" && order.price < orderStatus.result[0].H) {
+        //sell happened
+        wallet =
+            wallet +
+            order.price * order.quantity -
+            order.price * order.quantity * 0.0025;
+        orders.splice(index, 1);
+        index--;
+        current_order_count--;
+        happenedOrders.push({ order: order, stopLoss: false, wallet: wallet });
+        console.log("sell happened"+ JSON.stringify(order));
+        } else if (
+        order.side === "sell" &&
+        order.stopLossPrice > orderStatus.result[0].L
+        ) {
+        //stoploss worked
+        wallet =
+            wallet +
+            order.price * order.quantity -
+            order.price * order.quantity * 0.0025;
+        orders.splice(index, 1);
+        index--;
+        current_order_count--;
+        happenedOrders.push({ order: order, stopLoss: true, wallet: wallet });
+        }
+    } else {
+        //timeout remove order
+        orders.splice(index, 1);
+        index--;
+        if (order.side === "buy") {
+        wallet = wallet + order.price * order.quantity;
+        } else {
+        //hodl forever :D :D :D or sell it with current price
+        holdingOrders.push(order);
+        }
+    }
 
+    if (happenedOrders.length > 0) console.log(happenedOrders);
+    if (index < orders.length - 1) {
+        index++;
+        bittrexClient._getLatestTick(
+        {
+            marketName: orders[index].pair,
+            tickInterval: orders[index].interval,
+            index: index
+        },
+        recursive
+        );
+    }
+}
 
+setInterval(function() {
+  checkOrders();
+}, 1 * 60 * 1000);
